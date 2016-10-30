0c741e03d31aaf5bd327dd87f7302c0b8c3670b9
antonyoni <a@arbo.red>
2016-10-30 21:49:25 +0000

    Adding StreamSplitter class and Tests.

---
+++ StreamSplitter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace PsUtils {
+
+    public class StreamSplitter {
+
+        private static Encoding defaultEncoding = new UTF8Encoding();
+        private static int defaultBufferSize = 128 * 1024;
+
+        public static IEnumerable<byte[]> Split(Stream stream) {
+            var delimiter = defaultEncoding.GetBytes(Environment.NewLine);
+            return Split(stream, delimiter);
+        }
+
+        public static IEnumerable<byte[]> Split(Stream stream, byte[] delimiter) {
+
+            MemoryStream ms = new MemoryStream(defaultBufferSize * 2); // DocBuffer
+            var buffer = new byte[defaultBufferSize];
+            int readLength = 0;
+
+            while ((readLength = stream.Read(buffer, 0, buffer.Length)) > 0) {
+
+                var splitEnds = KMP.FindAll(buffer, delimiter);
+                var startPointer = 0;
+
+                for (int i = 0; i < splitEnds.Length; i++) {
+                    var endPointer = splitEnds[i];
+                    if (endPointer > readLength)
+                        break;
+                    var copyLength = endPointer - startPointer + delimiter.Length;
+                    byte[] doc;
+                    if (ms.Length > 0) {
+                        ms.Write(buffer, startPointer, copyLength);
+                        doc = ms.ToArray();
+                        ms = new MemoryStream(defaultBufferSize * 2);
+                    } else {
+                        doc = new byte[copyLength];
+                        Array.ConstrainedCopy(buffer, startPointer, doc, 0, copyLength);
+                    }
+                    yield return doc;
+                    startPointer = endPointer + delimiter.Length;
+                }
+
+                // handle remaining bytes
+                if (startPointer < readLength) {
+                    var copyLength = readLength - startPointer;
+                    ms.Write(buffer, startPointer, copyLength);
+                }
+
+            }
+
+            // handle anything left in the DocBuffer
+            if (ms.Length > 0) {
+                yield return ms.ToArray();
+                ms = null;
+            }
+
+        }
+
+    }
+
+}
---
+++ Tests/StreamSplitterTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace PsUtils.Tests {
+
+    [TestClass()]
+    public class StreamSplitterTests {
+
+        private string filePath1 = "..\\..\\Data\\file1.txt";
+        private string filePath2 = "..\\..\\Data\\file1_nonewline.txt";
+        private string filePath3 = "..\\..\\Data\\file2.txt";
+
+        [TestMethod()]
+        public void SplitTest_DefaultDelimiter_File1() {
+            using (var stream = new FileStream(filePath1, FileMode.Open)) {
+                var split = StreamSplitter.Split(stream);
+                Assert.AreEqual(10, split.Count());
+            }
+        }
+
+        [TestMethod()]
+        public void SplitTest_DefaultDelimiter_File2() {
+
+            using (var stream = new FileStream(filePath2, FileMode.Open)) {
+                // Check that if there's no new line at end of file, it still flushes the end
+                var split = StreamSplitter.Split(stream);
+                Assert.AreEqual(10, split.Count());
+            }
+        }
+
+        [TestMethod()]
+        public void SplitTest_CustomDelimiter_File3() {
+            using (var stream = new FileStream(filePath3, FileMode.Open)) {
+                var delimeter = Encoding.UTF8.GetBytes("</doc>");
+
+                var split = StreamSplitter.Split(stream, delimeter);
+
+                Assert.AreEqual(5, split.Count());
+            }
+        }
+
+        [TestMethod()]
+        public void SplitTest_DelimiterDoesNotExist_File3() {
+            using (var stream = new FileStream(filePath3, FileMode.Open)) {
+                var delimeter = Encoding.UTF8.GetBytes("empty");
+
+                var split = StreamSplitter.Split(stream, delimeter);
+
+                Assert.AreEqual(1, split.Count());
+            }
+        }
+
+    }
+
+}
