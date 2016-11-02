4483600334d1d42c1c7b4560dd86d25f9eb64b54
antonyoni <a@arbo.red>
2016-11-02 18:57:29 +0000

    Updating release config.

--- Tests/setup-test.ps1
+++
@@ -1,5 +0,0 @@
-$here = Split-Path -Parent $MyInvocation.MyCommand.Path
-$projectDir = Split-Path $here -Parent
-$modulePath = Join-Path $projectDir "bin\Debug\PsUtils.dll"
-
-Import-Module $modulePath
