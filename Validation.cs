4629699b4a10fca0fef8e96005a4189c9de60ee6
antonyoni <a@arbo.red>
2016-11-06 14:39:12 +0000

    Fixing relative path support in ValidatePath

--- Validation.cs
+++ Validation.cs
@@ -4,12 +4,27 @@ using System.Management.Automation;
 namespace PsUtils.Validation {
 
     public class ValidatePathAttribute : ValidateArgumentsAttribute {
+
         protected override void Validate(object arguments, EngineIntrinsics engineIntrinsics) {
-            string filePath = arguments as string;
-            if (filePath == null || !File.Exists(filePath)) {
+
+            string path = arguments as string;
+
+            if (path == null)
+                throw new ValidationMetadataException("Invalid path parameter.");
+
+            if (!Path.IsPathRooted(path)) {
+                path = Path.Combine(
+                    engineIntrinsics.SessionState.Path.CurrentFileSystemLocation.Path,
+                    path
+                );
+            }
+            
+            if (!(File.Exists(path) || Directory.Exists(path))) {
                 throw new FileNotFoundException("The specified Path does not exist.");
             }
+
         }
+
     }
 
 }
